7fb85c8afb58caf1bb7d3bf9df3c36af55510610
fjfj10 <kimmisuk2218@gmail.com>
2023-09-22 12:36:44 +0900

    design: OrBar 컴포넌트 디자인

---
+++ src/components/Layouts/SigninAndUpLayout/OrBar/Style.js
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+
+export const SOrBar = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 40px 14px;
+
+    & > span {
+        margin: 0px 18px;
+        font-size: 12px;
+        font-weight: 600;
+        color: #777;
+    }
+
+    &::after, &::before {
+        content: "";
+        flex-grow: 1;
+        border-bottom: 1px solid #dbdbdb;
+        height: 1px;
+    }
+`;
